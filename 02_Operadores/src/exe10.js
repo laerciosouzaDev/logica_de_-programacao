3ae8d6b3dbd563f161f65ba0947f7f82f8aaf7b9
laerciosouzaDev <laerciogplay@gmail.com>
2024-06-18 19:28:58 -0300

    senai

--- 02_Operadores/src/exe10.js
+++ 02_Operadores/src/exe10.js
@@ -4,27 +4,27 @@ var timeA = leia.questionInt("Digite a quantidade de gols time A: ")
 
 var timeB = leia.questionInt("digite a quantidade de gols time B:")
 
-var dif = (timeA - timeB)
+var dif = Math.abs(timeA - timeB)
 
-// switch(dif){
-//     case 0:
-//         console.log("EMPATE.")
-//     break;
-//     case 1:
-//     case 2:
-//     case 3:
-//         console.log("JOGO NORMAL!")
-//     break;
-//     default :
-//         console.log("QUE GOLEADA!!!")
+switch(dif){
+    case 0:
+        console.log("EMPATE.")
+    break;
+    case 1:
+    case 2:
+    case 3:
+        console.log("JOGO NORMAL!")
+    break;
+    default :
+        console.log("QUE GOLEADA!!!")
+}
+
+// if (dif === 0 ){
+//     console.log("EMPATE!!")
 // }
 
-if (dif === 0 ){
-    console.log("EMPATE!!")
-}
-
-if(dif  > 1 && 3 ){
-    console.log("JOGO NORMAL!")
-}else{
-    console.log("QUE GOLEADA!!!")
-}
+// if(dif  > 1 && 3 ){
+//     console.log("JOGO NORMAL!")
+// }else{
+//     console.log("QUE GOLEADA!!!")
+// }
---
+++ 02_Operadores/src/exe12.js
@@ -0,0 +1,4 @@
+import leia from "readline-sync"
+
+var timeA = leia.questionInt("time a")
+console.log(timeA)
