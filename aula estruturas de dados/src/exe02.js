b5dcdb735399e35ca9c9221efde7ffbb2c19edae
laerciosouzaDev <laerciogplay@gmail.com>
2024-07-02 21:49:30 -0300

    inicio exercicio

---
+++ aula estruturas de dados/src/exe02.js
@@ -0,0 +1,17 @@
+import leia from "readline-sync"
+
+var num = []
+
+
+
+for(var i = 0; i < 3; i++){
+    num[i] = leia.questionInt("digite valor: ")
+    console.log(num)
+
+}
+var valor = 0
+
+for(var j = 0; j < num.length; i++){
+    console.log(num[j])
+
+}
