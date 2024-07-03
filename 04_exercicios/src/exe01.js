da28da5cd7754c8ac0ac93445d6902ad1b123235
laerciosouzaDev <laerciogplay@gmail.com>
2024-07-02 21:50:50 -0300

    exes

---
+++ 04_exercicios/src/exe01.js
@@ -0,0 +1,13 @@
+import leia from "readline-sync"
+
+var nome = []
+
+var notas = []
+
+for(var i = 0; i < 3; i++){
+    nome[i] = leia.question("digite o nome do aluno: ")
+}
+
+
+
+
---
+++ 04_exercicios/src/exemple02.js
@@ -0,0 +1,55 @@
+// var lista = ["Laercio Souza", 35, true];
+
+
+// // console.log(lista [0] , lista[2]);
+
+// console.log(lista.length);
+
+
+// ******************************
+// preenchendo minha lista
+
+// var lista = ["teste"];
+
+// console.log(lista)
+
+// lista.push("Laercio Souza")
+
+// lista.push(35)
+
+// lista.push(true)
+
+// console.log(lista)
+
+// **********
+
+// percorrer minha lista com estrutura de repeticao
+// var lista = []
+// for (var i =0; i < 10; i++){
+//     lista[i] = "laercio"
+// }
+// console.log(lista);
+
+// var i = 0
+
+// while(i < 10){
+//     lista[i] = leia.question("digite: ")
+//     i++;
+// }
+
+// //percorrer a lista
+
+// for(var j = 0; j < lista.length; j++){
+//     console.log(lista[j])
+//     if(lista[j] === "laercio"){
+//         console.log(lista[j])
+//     }
+// }
+
+// especifico para vetores
+
+for(let valor of lista){
+    if(valor.includes(sta)){
+        console.log(valor)
+    }
+}
